f2b3f32b94816670bf9470ad6e16385bd38e93ef
s3698291 <41663801+s3698291@users.noreply.github.com>
2020-08-18 12:03:09 +0800

    Create view-account.php

---
+++ view-account.php
@@ -0,0 +1,61 @@
+<?php 
+session_start();
+require_once 'config.php';
+
+$name = $email = $role = $password = $deactivate = "";
+$name_eror = $email_error = $role_error = $password_error = $confirm_password_error = $deactivate_error = "";
+
+?>
+
+<html>
+
+<head>
+    <title>View Account | Tour Management</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- JavaScript from Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <!-- CSS from Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+
+    <!-- Individualised CSS -->
+    <link rel="stylesheet" type="text/css" href="style/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- Register Account field -->
+
+    <h1 class="text-center mt-3"> View Account </h1>
+
+    <div class="container">
+        <form method="POST">
+            <label for="locationId">Location</label>
+
+            <select class="form-control <?php echo (!empty($tour_id_err)) ? 'border border-danger' : ''; ?>" id="locationId" name="locationId">
+                <option value="" selected>Select location</option>
+            </select>
+
+            <button type="submit" class="btn btn-primary btn-block">Delete Location</button>
+        </form>
+
+    </div>
+
+    <!-- Register Account field -->
+
+    <?php include 'footer.php'; ?>
+</body>
+
+<<<<<<< HEAD:remove-location.php
+</html>
+
+<?php mysqli_close($conn); ?>
+=======
+</html>
+>>>>>>> jason:view-account.php
