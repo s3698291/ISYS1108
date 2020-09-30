2d0b0f22b976f23bc81dd9fd54d2b68aff30427c
s3730387 <42113257+s3730387@users.noreply.github.com>
2020-09-30 21:26:25 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-<div class="fixed-container text-white py-2 bg-back">
-        <h6 class="text-center mb-0">Copyright &copy; 2020</h6>
+<div class="fixed-container text-white py-2 bg-back d-flex footer">
+	<h6 class="text-center m-auto">Copyright &copy; 2020</h6>
 </div>
